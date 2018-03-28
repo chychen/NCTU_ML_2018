594d3fe18dd54e4ec34c759bc50ef1d8d833eb52
Jay Chen <jaycase.cs01@gmail.com>
2018-03-28 11:05:39 +0800

    done

---
+++ HW2/online_learning.py
@@ -0,0 +1,54 @@
+"""
+2. Online learning 
+Use online learning to learn the beta distribution of the parameter p (chance to see 1) of the coin tossing trails in batch. 
+    INPUT: 
+            i. A file contains many lines of binary outcomes: 
+                  0101010111011011010101 
+                  0110101 
+                  010110101101 
+                   ... 
+             ii. parameter a for the initial beta prior 
+             iii. parameter b for the initial beta prior 
+"""
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+
+def online_learning(all_data, prior_a, prior_b):
+    """ Use Beta-Binomial conjugation to perform online learning.
+    Print out the Binomial likelihood (based on MLE, of course), Beta prior and posterior probability (parameters only) for each line. 
+    """
+    prior = prior_a/(prior_a+prior_b)
+    for i, data in enumerate(all_data):
+        n = len(data)
+        successes = 0
+        for v in data:
+            if v == 1:
+                successes += 1
+        failures = n - successes
+        p = successes/n
+        mle = n * p
+        posterior = (successes+1)/(successes+1+failures+1)
+        print('DATA INDEX: {}\n\tMLE: {}\n\tPRIOR: {}\n\tPOSTERIOR: {}\n'.format(
+            i, mle, prior, posterior))
+        prior = posterior
+
+
+def main():
+    all_data = []
+    with open('online_learning_data.txt', 'r') as file_:
+        for line in file_.readlines():
+            temp = []
+            for c in line:
+                if c == '\n':
+                    break
+                else:
+                    temp.append(int(c))
+            all_data.append(temp)
+
+    online_learning(all_data, prior_a=1.0, prior_b=1.0)
+
+
+if __name__ == '__main__':
+    main()
