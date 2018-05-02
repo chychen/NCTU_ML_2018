5a9aac1d01817e7a07c9cd9f4b2d710af27483c1
Jay Chen <jaycase.cs01@gmail.com>
2018-05-02 19:22:59 +0800

    final

--- HW4/EM_algorithm.py
+++ HW4/EM_algorithm.py
@@ -46,7 +46,7 @@ def preprocessing(train_images):
     return train_images
 
 
-def em_algorithm(train_images, theta, lambda_, iterations=1e6, epsilon=1e-15):
+def em_algorithm(train_images, train_labels, theta, lambda_, iterations=1e6, epsilon=1e-15):
     """
     Expectation step:
         theta : shape=[10, image_size]
@@ -65,136 +65,90 @@ def em_algorithm(train_images, theta, lambda_, iterations=1e6, epsilon=1e-15):
     """
     N = train_images.shape[0]
     image_size = train_images.shape[1] * train_images.shape[2]
-    # M = []
-    # for i in range(N):
-    #     tmp = 0
-    #     for row_idx in range(train_images.shape[1]):
-    #         for col_idx in range(train_images.shape[2]):
-    #             tmp += train_images[i, row_idx, col_idx]
-    #     M.append(tmp)
 
-    for iter_idx in range(int(3)):
+    for iter_idx in range(int(20)):
         print('iteration index: ', iter_idx)
         old_theta = copy.deepcopy(theta)
-        theta = Mat.zeros(shape=[10, image_size]) + 1e-8 # avoid zeros
+        theta = Mat.zeros(shape=[10, image_size]) + 1e-8  # avoid zeros
         sum_w = [1e-8 for _ in range(10)]
+        majority_vote = Mat([[0 for _ in range(10)] for _ in range(10)])
         # Expectation Step
         for i in range(N):
-            weights = Mat.zeros([train_images.shape[0], 10])
+            weights = Mat.zeros([train_images.shape[0], 10]) + 1e-8
             for z in range(10):
                 logprob = 0.0
                 for row_idx in range(train_images.shape[1]):
                     for col_idx in range(train_images.shape[2]):
                         logprob += math.log(old_theta[z, row_idx*28+col_idx])*train_images[i, row_idx, col_idx] + math.log(
                             1-old_theta[z, row_idx*28+col_idx])*(1-train_images[i, row_idx, col_idx])
-                        
+
                     logprob += math.log(lambda_[z])
                     weights[i, z] = logprob
                     # input(weights[i, z])
             max_weight = [-1e10 for _ in range(N)]
 
             for z in range(10):
-                if max_weight[i] < weights[i,z]:
-                    max_weight[i] = weights[i,z]
+                if max_weight[i] < weights[i, z]:
+                    max_weight[i] = weights[i, z]
 
             sum_ = 0.0
             for z in range(10):
-                weights[i,z] -= max_weight[i] # normalized
-                weights[i,z] = math.exp(weights[i,z])
+                weights[i, z] -= max_weight[i]  # normalized
+                weights[i, z] = math.exp(weights[i, z])
                 # input(weights[i,z])
-                sum_ += weights[i,z]
+                sum_ += weights[i, z]
             for z in range(10):
-                weights[i,z] /= sum_
-                
+                weights[i, z] /= sum_
+
             for z in range(10):
                 sum_w[z] += weights[i, z]
                 for pixels in range(image_size):
                     theta[z, pixels] += weights[i, z] * \
                         train_images[i, pixels//28, pixels % 28]
 
+            # label groups by majority vote
+            predict_result = -1
+            max_weight = -1e10
+            for z in range(10):
+                if weights[i, z] > max_weight:
+                    max_weight = weights[i, z]
+                    predict_result = z
+            for z in range(10):
+                majority_vote[predict_result, train_labels[i]] += 1
+
         # Maximization Step
         for z in range(10):
             lambda_[z] = sum_w[z]/N
             for pixels in range(image_size):
                 theta[z, pixels] /= sum_w[z]
-                if theta[z, pixels]>=1.0:
-                    # input(theta[z, pixels])
-                    theta[z, pixels] =1.0-1e-8
-        print(lambda_)
-        print(theta)
+                if theta[z, pixels] >= 1.0:
+                    theta[z, pixels] = 1.0-1e-8
+
+        # show mean
+        for z in range(10):
+            print()
+            for row_idx in range(28):
+                str_ = ''
+                for col_idx in range(28):
+                    if int(theta[z,row_idx*28+col_idx] > 0.5) == 0:
+                        str_ += '0 '
+                    else:
+                        str_ += '  '
+                print(str_)
         # if mean_abs_diff(old_theta, theta) < epsilon:
         #     break
-
-    return weights
-
-    # cluster_n = 10
-    # px_n = 784
-    # img_n = 100
-    # alpha = 0.00000008
-
-    # pi = [0.1 for i in range(cluster_n)]
-    # mu = [[np.random.uniform(0, 1) for i in range(px_n)] for j in range(cluster_n)]
-    # z  = [[0.1 for i in range(cluster_n)] for j in range(img_n)]
-
-    # for idx in range(20):
-
-    #     Nm = [0 for i in range(cluster_n)]
-    #     mean = [[0.0 for i in range(px_n)] for j in range(cluster_n)]
-    #     predict = [-1 for i in range(img_n)]
+        group_labels = [-1 for _ in range(10)]
         
-    #     for n in range(img_n):
+        for z in range(10):
+            label = -1
+            max_vote = -1e10
+            for l in range(10):
+                if majority_vote[z, l]> max_vote:
+                    max_vote = majority_vote[z, l]
+                    label = l
+            group_labels[z] = label
 
-    #         # Expectation step
-    #         for k in range(cluster_n):
-    #             for i in range(px_n):
-    #                 z[n][k] += train_images[n, i//28, i%28] * math.log(mu[k][i]) + (1 - train_images[n, i//28, i%28]) * math.log(1.0 - mu[k][i])
-    #             z[n][k] += math.log(pi[k])
-
-    #         z_0 = z[n][0]
-    #         # divided by first element
-    #         temp_max = max(z[n])
-    #         temp_max_idx = 0
-    #         for k in range(cluster_n):
-    #             if temp_max == z[n][k]:
-    #                 temp_max_idx = k
-
-    #         for k in range(cluster_n):
-    #             z[n][k] -= z[n][temp_max_idx]
-                
-    #         for k in range(cluster_n):
-    #             try:
-    #                 z[n][k] = math.exp(z[n][k])
-    #             except:
-    #                 z[n][k] = 0.0
-            
-    #         # normalize
-    #         z_sum = sum(z[n])
-    #         z[n] = [z_k / z_sum for z_k in z[n]]
-
-    #         # predict
-    #         temp_max = 0
-    #         predict_k = 0
-    #         for k in range(1, cluster_n):
-    #             if z[n][k] > temp_max:
-    #                 temp_max = z[n][k]
-    #                 predict_k = k
-                    
-    #         predict[n] = predict_k
-
-    #         # Maximization step
-    #         for k in range(cluster_n):
-    #             Nm[k] += z[n][k]
-    #             for i in range(px_n):
-    #                 mean[k][i] += z[n][k] * train_images[n, i//28, i%28]
-        
-    #     for k in range(cluster_n):
-    #         pi[k] = float(Nm[k] + alpha) / float(img_n + alpha * cluster_n)
-    #         for i in range(px_n):
-    #             mean[k][i] += alpha
-    #             mean[k][i] /= Nm[k] + alpha * img_n
-    #     mu = mean
-    #     print(mean)
-    # return Mat(z)
+    return weights, group_labels
 
 
 def main():
@@ -204,21 +158,24 @@ def main():
     # test_images, test_labels = load_mnist(dataset='testing', fetch_size=10000)
 
     train_images = preprocessing(train_images)
+    for i in range(28):
+        print(train_images[0, i])
+    print(train_labels[0])
     init_theta = Mat([[np.random.uniform(0.0, 1.0) for _ in range(train_images.shape[1]
-                                          * train_images.shape[2])] for _ in range(10)])
+                                                                  * train_images.shape[2])] for _ in range(10)])
     # init_theta = Mat([[0.5 for _ in range(train_images.shape[1]
     #                                       * train_images.shape[2])] for _ in range(10)])
     init_lambda = [0.1 for _ in range(10)]
-    logits = em_algorithm(train_images, init_theta, init_lambda)
+    logits, group_labels = em_algorithm(train_images, train_labels, init_theta, init_lambda)
     prediction = []
     for i in range(logits.shape[0]):
         largest_idx = 0
         for z in range(logits.shape[1]):
-            print(logits[i, z])
+            # print(logits[i, z])
             if logits[i, z] > logits[i, largest_idx]:
                 largest_idx = z
         # input()
-        prediction.append([largest_idx])
+        prediction.append([group_labels[largest_idx]])
     prediction = Mat(prediction)
     train_labels = Mat([train_labels]).t()
     # confusion matrix
