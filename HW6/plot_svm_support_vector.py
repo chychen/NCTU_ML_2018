08ac065f474ed84f591487f7d676a7d155523c51
Jay Chen <jaycase.cs01@gmail.com>
2018-06-07 16:48:28 +0800

    tmp

--- HW6/plot_svm_support_vector.py
+++ HW6/plot_svm_support_vector.py
@@ -7,13 +7,15 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
+import os
+import numpy as np
 from libsvm.python.svmutil import *
 from libsvm.tools.grid import *
 from svmutil import *
 from grid import *
 from pca import pca_plot_with_svm
 from data_loader import load_mnist
-import numpy as np
+
 
 def test_kernel(mode):
     """
@@ -26,7 +28,8 @@ def test_kernel(mode):
     m = svm_train(y, x, '-s 0 -t {} -c 1'.format(mode))
     # get support vector
     support_vectors = m.get_SV()
-    p_labels, p_accs, p_vals = svm_predict(np.zeros(len(support_vectors)), support_vectors, m)
+    p_labels, p_accs, p_vals = svm_predict(
+        np.zeros(len(support_vectors)), support_vectors, m)
     # sparse to dense
     dense_sv = np.zeros(shape=[len(support_vectors), 28*28])
     for i, dict_ in enumerate(support_vectors):
@@ -34,7 +37,9 @@ def test_kernel(mode):
             dense_sv[i, key] = dict_[key]
     # vis
     data = load_mnist()
-    pca_plot_with_svm(data['X_train'], data['T_train'], dense_sv, np.array(p_labels), file_name='svm_pca_mode{}'.format(mode))
+    pca_plot_with_svm(data['X_train'], data['T_train'], dense_sv, np.array(
+        p_labels), file_name='svm_pca_mode{}'.format(mode))
+
 
 def precomputed_kernel():
     data = load_mnist()
@@ -49,22 +54,30 @@ def precomputed_kernel():
         return linear_kernel(x1, x2) + RBF_kernel(np.array(x1), np.array(x2), gamma)
 
     # sparse
-    gram_matrix = []
-    for i, x1 in enumerate(data['X_train']):
-        print(i)
-        tmp = {}
-        tmp[0] = i+1
-        for j, x2 in enumerate(data['X_train']):
-            tmp[j+1] = new_kernel(x1, x2, gamma=1.0/28)
-        gram_matrix.append(tmp)
-    test_gram_matrix = []
-    for i, x1 in enumerate(data['X_test']):
-        print(i)
-        tmp = {}
-        tmp[0] = i  # any number
-        for j, x2 in enumerate(data['X_train']):
-            tmp[j+1] = new_kernel(x1, x2, gamma=1.0/28)
-        test_gram_matrix.append(tmp)
+    if os.path.exists('data/gram_matrix_train.npy'):
+        gram_matrix = np.load('data/gram_matrix_train.npy')
+    else:
+        gram_matrix = []
+        for i, x1 in enumerate(data['X_train']):
+            print(i)
+            tmp = {}
+            tmp[0] = i+1
+            for j, x2 in enumerate(data['X_train']):
+                tmp[j+1] = new_kernel(x1, x2, gamma=1.0/28)
+            gram_matrix.append(tmp)
+        np.save('data/gram_matrix_train.npy', gram_matrix)
+    if os.path.exists('data/gram_matrix_test.npy'):
+        test_gram_matrix = np.load('data/gram_matrix_test.npy')
+    else:
+        test_gram_matrix = []
+        for i, x1 in enumerate(data['X_test']):
+            print(i)
+            tmp = {}
+            tmp[0] = i  # any number
+            for j, x2 in enumerate(data['X_train']):
+                tmp[j+1] = new_kernel(x1, x2, gamma=1.0/28)
+            test_gram_matrix.append(tmp)
+        np.save('data/gram_matrix_test.npy', test_gram_matrix)
 
     # train
     prob = svm_problem(data['T_train'], gram_matrix)
@@ -72,14 +85,16 @@ def precomputed_kernel():
     m = svm_train(prob, param)
     # get support vector
     support_vectors = m.get_SV()
-    p_labels, p_accs, p_vals = svm_predict(np.zeros(len(support_vectors)), support_vectors, m)
+    p_labels, p_accs, p_vals = svm_predict(
+        np.zeros(len(support_vectors)), support_vectors, m)
     # sparse to dense
     dense_sv = np.zeros(shape=[len(support_vectors), 28*28])
     for i, dict_ in enumerate(support_vectors):
         for key in dict_.keys():
             dense_sv[i, key] = dict_[key]
     # vis
-    pca_plot_with_svm(data['X_train'], data['T_train'], dense_sv, np.array(p_labels), file_name='svm_pca_mode_precomputed')
+    pca_plot_with_svm(data['X_train'], data['T_train'], dense_sv, np.array(
+        p_labels), file_name='svm_pca_mode_precomputed')
 
 
 def main():
